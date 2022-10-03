0fc14f72941716e54a2c8b950213d0ab4f43e808
efletcher99 <ethanfletch99@gmail.com>
2022-10-03 08:15:36 -0600

    finished

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+struct Node{
+    int key;
+    struct Node *right;
+    struct Node *left;
+};
+
+struct Node* newNode(int data){
+
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->key = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp; 
+}
+
+int factorial(int num){
+    int fact = 1;
+    if(num == 0){
+        return 1;
+    }
+    else {
+        while(num > 1){
+            fact = fact * num;
+            num--;
+        }
+    }
+    return fact;
+}
+
+int numOfBST(int key) {    
+        int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));    
+        return catalanNumber;    
+} 
+
+int main(){
+    struct Node* root = NULL;
+    int factorialNum = 6;
+    int num = factorial(factorialNum);
+    printf("The factorial of %d is: %d", factorialNum, num);
+    return 0;  
 }
